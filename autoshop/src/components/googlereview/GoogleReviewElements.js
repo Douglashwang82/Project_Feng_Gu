2c7951998208d6bcb5b585d4132957deef427979
douglashwang <douglas821209@gmail.com>
2022-04-26 13:38:40 -0700

    update:google review

--- autoshop/src/components/googlereview/GoogleReviewElements.js
+++ autoshop/src/components/googlereview/GoogleReviewElements.js
@@ -25,6 +25,9 @@ export const ReviewFrame = styled.div`
     padding-top:10px;
     position:relative;
     z-index:1;
+    background-color: rgba(255, 255, 255, .15);  
+ backdrop-filter: blur(5px);
+ color:white;
     
 `
 export const ReviewSection = styled.div`
@@ -81,6 +84,7 @@ export const ReviewArea = styled.div`
     position: relative;
     border-bottom-left-radius: 50px;
     border-bottom-right-radius: 50px;
+    ${'' /* background: radial-gradient(circle at -26% -36%, lightyellow, darkblue); */}
 `
 export const ReviewMiddle = styled.div`
 
@@ -105,24 +109,22 @@ border-bottom-left-radius:10px;
 `
 
 export const ReviewShadow3 = styled.div`
-padding-right:10px;
+${'' /* padding-right:10px;
 padding-bottom:10px;
 // background:lightgreen;
 border-top-right-radius: 15px;
 border-top-left-radius: 15px;
 border-bottom-right-radius:15px;
 border-bottom-left-radius:12px;
-background: radial-gradient(circle at -26% -36%, lightblue, darkblue);
+background: radial-gradient(circle at -26% -36%, grey, #BABABA); */}
 
 `
 
 export const ReviewGlass = styled.div`
-    background:grey;
-    position: relative;
+    ${'' /* position: relative;
     z-index: 20;
     width:100%;
     height:100%;
-    top: -100%;
-    opacity: 0.5;
+    top: -100%; */}
     
 `
